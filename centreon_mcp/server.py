f650493b81120d2fa4fce1f8ed6e9d5d13a28564
gregory-lvtx <gleventoux@centreon.com>
2026-01-23 15:31:35 +0100

    Add centreon exception more details

--- centreon_mcp/server.py
+++ centreon_mcp/server.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastmcp import FastMCP
 
+from centreon_mcp import CREDENTIALS
 from centreon_mcp.utils.logger import logger
 from centreon_mcp.utils.request import CentreonAPIError, request
 
@@ -11,12 +12,16 @@ async def lifespan(app: FastMCP):
     """
     Lifespan context manager for FastMCP application.
     """
+    # Check if Centreon API token is provided
+    if not CREDENTIALS["CENTREON_API_TOKEN"]:
+        msg = "Centreon API token is missing. Don't starting MCP server."
+        raise RuntimeError(msg)
+
     # Test Centreon API connectivity
     try:
         result = await request("GET", "platform/versions")
-    except CentreonAPIError:
-        logger.error("Failed to connect to Centreon API")
-        raise
+    except CentreonAPIError as e:
+        raise e
     else:
         version = result["web"]["version"]
         logger.info(f"Connected to Centreon API version {version}")
--- centreon_mcp/utils/request.py
+++ centreon_mcp/utils/request.py
@@ -8,13 +8,29 @@ class CentreonAPIError(Exception):
     Custom exception for Centreon API errors.
     """
 
-    pass
+    def __init__(self, status: int, url: str, method: str, content: dict) -> None:
+        self.status = status
+        self.url = url
+        self.method = method
+        self.content = content
+
+    def __str__(self) -> str:
+        """
+        Return string representation of the error.
+        """
+        content = "\n".join(f"  {key}: {value}" for key, value in self.content.items())
+        return (
+            f"\nCentreon API Error [{self.status}]"
+            f"\nMethod: {self.method}"
+            f"\nURL: {self.url}"
+            f"\nContent:\n{content}"
+        )
 
 
 async def request(
     method: str,
     endpoint: str,
-    data: dict | None = None,
+    json: dict | None = None,
     params: dict | None = None,
     timeout: float | None = None,
 ) -> dict:
@@ -32,9 +48,12 @@ async def request(
     try:
         async with httpx.AsyncClient() as client:
             response = await client.request(
-                method, url, headers=headers, data=data, params=params, timeout=timeout
+                method, url, headers=headers, json=json, params=params, timeout=timeout
             )
+            content = response.json()
             response.raise_for_status()
-            return response.json()
-    except (httpx.RequestError, httpx.HTTPStatusError):
-        raise CentreonAPIError()
+            return content
+    except httpx.HTTPStatusError as e:
+        status = e.response.status_code
+        url = str(e.request.url)
+        raise CentreonAPIError(status, url, method, content)

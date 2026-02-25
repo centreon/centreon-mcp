f4768585df9c59865ce18d5762880faf0581c7fa
gregory-lvtx <gleventoux@centreon.com>
2026-02-25 16:31:06 +0100

    Remove old host and service tools

--- centreon_mcp/components/__init__.py
+++ centreon_mcp/components/__init__.py
@@ -1,15 +1,13 @@
 from centreon_mcp.components.acknowledgement import acknowledgement
 from centreon_mcp.components.comment import comment
 from centreon_mcp.components.downtime import downtime
-from centreon_mcp.components.host import host
 from centreon_mcp.components.hostgroup import hostgroup
 from centreon_mcp.components.monitoring_server import monitoring_server
-from centreon_mcp.components.service import service
+from centreon_mcp.components.resource import resource
 from centreon_mcp.components.servicegroup import servicegroup
 
 components = [
-    host,
-    service,
+    resource,
     hostgroup,
     servicegroup,
     monitoring_server,

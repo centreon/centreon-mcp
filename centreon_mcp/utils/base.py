e46512577e48c2d96c8d9bbcbda4dabcb3e75745
gregory-lvtx <gleventoux@centreon.com>
2026-04-14 15:47:27 +0200

    Remove virtual links

--- centreon_mcp/utils/base.py
+++ centreon_mcp/utils/base.py
@@ -1,6 +1,5 @@
-import asyncio
 import json
-from typing import ClassVar, Literal, Type, TypeVar
+from typing import Literal, Type, TypeVar
 
 from pydantic import BaseModel
 
@@ -21,45 +20,6 @@ class BaseOrder(BaseModel):
 
 
 class BaseFilter(BaseModel):
-    links: ClassVar[list[ConstraintLink]] = []
-
-    async def fill(self, link: ConstraintLink) -> None:
-        """
-        Fill filter field id using field name and class.
-        """
-        # If the objet id is already set, do nothing.
-        if getattr(self, f"{link.object}_id", None) is not None:
-            return
-
-        # Generate conditions for searching the item based on provided fields.
-        conditions: list[dict] = []
-        for field in link.fields:
-            value = getattr(self, f"{link.object}_{field}")
-            if value is not None:
-                conditions.append({field: {"$eq": value}})
-
-        # If no conditions are generated, objet don't matter in the filter.
-        if len(conditions) == 0:
-            return
-
-        # Else, search for the objet id using conditions
-        items = await link.cls.list(search=json.dumps({"$and": conditions}))
-
-        # If no item is found, raise an error with the conditions used for searching.
-        if len(items) == 0:
-            msg = f"{link.cls.__name__} with with conditions {conditions} not found."
-            raise ValueError(msg)
-
-        # Else, set the objet id attribute with the found item's id.
-        setattr(self, f"{link.object}_id", getattr(items[0], "id"))
-
-    async def complete(self) -> None:
-        """
-        Compute filters based on fields not available in Centreon API.
-        """
-        tasks = [asyncio.create_task(self.fill(link)) for link in self.links]
-        await asyncio.gather(*tasks)
-
     @property
     def conditions(self) -> list:
         """
@@ -88,7 +48,6 @@ async def _list(
     """
     filters = filters or []
     order = order or order_cls()
-    await asyncio.gather(*(filter.complete() for filter in filters))
     conditions = (
         {
             "$or": [

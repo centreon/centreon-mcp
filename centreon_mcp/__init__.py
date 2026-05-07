4a9cc7fc36b21aff2e67ebf2e461bf4536bd8967
gregory-lvtx <gleventoux@centreon.com>
2026-05-07 17:09:59 +0200

    Load env variable from .env file

--- centreon_mcp/__init__.py
+++ centreon_mcp/__init__.py
@@ -1,9 +1,8 @@
 import os
-from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv(Path(__file__).parent / ".env")
+load_dotenv()
 
 CREDENTIALS = {
     name: (os.environ.get(name) or default)

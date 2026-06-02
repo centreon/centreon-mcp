ff8931db68b40e7fd2918802e33a435233d0b974
gregory-lvtx <gleventoux@centreon.com>
2026-06-02 16:13:42 +0200

    Use _list for service groups

--- tests/components/test_component_servicegroup.py
+++ tests/components/test_component_servicegroup.py
@@ -31,7 +31,7 @@ async def test_list_servicegroups(logger: MagicMock, _list: AsyncMock):
     results = await list_servicegroups(filters, limit, page, order)
 
     # Assert _list called with right args
-    _list.assert_awaited_once_with(ServiceGroup, ServiceGroupOrder, filters, limit, page, order)
+    _list.assert_awaited_once_with(ServiceGroup, filters, limit, page, order)
 
     # Assert result
     assert results[0] == servicegroup

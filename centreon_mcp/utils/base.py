b48046ba6a087809bf8d7f38e5dfaac0e42e1816
gregory-lvtx <gleventoux@centreon.com>
2026-06-03 11:34:56 +0200

    Remove old _list method

--- centreon_mcp/utils/base.py
+++ centreon_mcp/utils/base.py
@@ -1,10 +1,7 @@
-import json
 from collections.abc import Sequence
 from typing import Literal
 
 from pydantic import BaseModel
-
-from centreon_mcp.utils.mixins import ListMixin
 
 
 class BaseOrder(BaseModel):
@@ -32,20 +29,3 @@ class BaseFilter(BaseModel):
                 if value is not None
             }.items()
         ]
-
-
-async def _list[CentreonModel: ListMixin, OrderType: BaseOrder, FilterType: BaseFilter](
-    model: type[CentreonModel],
-    order_cls: type[OrderType],
-    filters: list[FilterType] | None = None,
-    limit: int = 10,
-    page: int = 1,
-    order: OrderType | None = None,
-) -> list[CentreonModel]:
-    """
-    Generic function to list ressources in real-time monitoring based on provided filters, pagination and order
-    """
-    order = order or order_cls()
-    search = json.dumps(BaseFilter.join(filters))
-    sort_by = order.model_dump_json()
-    return await model.list(search, limit, page, sort_by)

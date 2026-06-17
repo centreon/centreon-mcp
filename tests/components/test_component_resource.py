b4fd51dfeecc97fb17cde97ef15e2970f1009cd1
gregory-lvtx <gleventoux@centreon.com>
2026-06-17 15:51:51 +0200

    Move type resource

--- tests/components/test_component_resource.py
+++ tests/components/test_component_resource.py
@@ -2,7 +2,7 @@ import json
 from unittest.mock import AsyncMock, MagicMock, patch
 
 from centreon_mcp.components.resource import ResourceFilter, ResourceOrder, list_resources
-from centreon_mcp.types.resource import Resource
+from centreon_mcp.types.monitoring.resource import Resource
 
 MODULE = "centreon_mcp.components.resource"
 

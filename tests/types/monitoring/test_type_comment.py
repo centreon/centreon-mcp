6f149a6d2e4d8fab136eee83b0b3473ee46eb5f1
gregory-lvtx <gleventoux@centreon.com>
2026-06-17 15:48:58 +0200

    Move type comment

--- tests/types/monitoring/test_type_comment.py
+++ tests/types/monitoring/test_type_comment.py
@@ -1,8 +1,8 @@
 from unittest.mock import AsyncMock, patch
 
-from centreon_mcp.types.comment import Comment, CommentResource
+from centreon_mcp.types.monitoring.comment import Comment, CommentResource
 
-MODULE = "centreon_mcp.types.comment"
+MODULE = "centreon_mcp.types.monitoring.comment"
 
 
 @patch(f"{MODULE}.request", new_callable=AsyncMock)

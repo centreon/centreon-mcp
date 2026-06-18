ead59dbb098a7c89fe391d33b999dce73941702c
gregory-lvtx <gleventoux@centreon.com>
2026-06-18 15:29:21 +0200

    Move filter and order for service

--- tests/components/test_component_service.py
+++ tests/components/test_component_service.py
@@ -1,8 +1,8 @@
 import json
 from unittest.mock import AsyncMock, MagicMock, patch
 
-from centreon_mcp.components.service import ServiceFilter, count_services_by_status
-from centreon_mcp.types.monitoring.service import ServiceStatusCount
+from centreon_mcp.components.service import count_services_by_status
+from centreon_mcp.types.monitoring.service import ServiceFilter, ServiceStatusCount
 
 MODULE = "centreon_mcp.components.service"
 

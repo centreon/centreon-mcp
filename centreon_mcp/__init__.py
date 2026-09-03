67f96a48ab40cfc416e7859868d058cf6412aab5
gregory-lvtx <gleventoux@centreon.com>
2026-09-03 11:13:15 +0200

    fix: default centreon api token to empty string and pass it through compose

--- centreon_mcp/__init__.py
+++ centreon_mcp/__init__.py
@@ -8,7 +8,7 @@ CREDENTIALS = {
     name: (os.environ.get(name) or default)
     for name, default in [
         ("CENTREON_BASE_URL", ""),
-        ("CENTREON_API_TOKEN", None),
+        ("CENTREON_API_TOKEN", ""),
         ("CENTREON_MCP_LOG_LEVEL", "INFO"),
         ("CENTREON_MCP_HOST", "localhost"),
         ("CENTREON_MCP_PORT", "8000"),

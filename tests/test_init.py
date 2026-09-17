b48af369358037606db83c19aa6b669160489dad
gregory-lvtx <gleventoux@centreon.com>
2026-09-17 14:54:08 +0200

    test(config): add coverage for CA bundle existence validation

--- tests/test_init.py
+++ tests/test_init.py
@@ -13,3 +13,9 @@ def test_settings_verify(tls_secure: bool, ca_bundle: str | None, verify: bool):
         base_url="http://localhost:4000/centreon", tls_secure=tls_secure, ca_bundle=ca_bundle
     )
     assert settings.verify == verify
+
+
+def test_settings_ca_bundle_not_exists():
+
+    with pytest.raises(ValueError):
+        _ = Settings(tls_secure=True, ca_bundle="/fake/path")
